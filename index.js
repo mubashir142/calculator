a400554d107ea16f0a84f970bf4262a2082dd56e
muhammad mubashir <qmubashir2003@gmail.com>
2025-02-15 22:40:55 +0500

    Added modulus and exmponentiation functions

--- index.js
+++ index.js
@@ -9,3 +9,13 @@ ReactDOM.render(
     </React.StrictMode>,
     document.getElementById('root')
 );
+function modulus(a, b) {
+    return a % b;
+}
+
+function exponentiation(a, b) {
+    return Math.pow(a, b);
+}
+
+console.log(modulus(10, 3)); // Output: 1
+console.log(exponentiation(2, 3)); // Output: 8
